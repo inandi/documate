31c2f2c11f34fbe7b16f35cb8f086565283dde53
Gobinda Nandi <gobinda.nandi@entrata.com>
2025-03-03 19:12:43 +0530

    add year

--- extension.js
+++ extension.js
@@ -5,13 +5,15 @@ function activate(context) {
         const config = vscode.workspace.getConfiguration('autofillAuthor');
         const author = config.get('author', 'Unknown Author');
         const version = config.get('version', '1.0.0');
+        const year = new Date().getFullYear();
+        const snippet = `/**\n * @author ${author}\n * @version ${version}\n * @since ${year}\n * @copyright © ${year} ${author}. All rights reserved.\n */`;
 
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
         }
 
-        const snippet = `/**\n * @author ${author}\n * @version ${version}\n */`;
+        // const snippet = `/**\n * @author ${author}\n * @version ${version}\n */`;
         editor.insertSnippet(new vscode.SnippetString(snippet));
         vscode.window.showInformationMessage(snippet);
 
