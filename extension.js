bc15a5f7e26ec42cf7011437004adeeaeb52fe5a
Gobinda Nandi <gobinda.genx@yahoo.in>
2025-03-06 00:44:12 +0530

    add class doc

--- extension.js
+++ extension.js
@@ -1,25 +1,80 @@
 const vscode = require('vscode');
 
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('extension.insertAuthorInfo', function () {
-        const config = vscode.workspace.getConfiguration('documate');
-        const author = config.get('author', 'Unknown Author');
-        const version = config.get('version', '1.0.0');
-        const year = new Date().getFullYear();
-        const snippet = `/**\n * @author ${author}\n * @version ${version}\n * @since ${year}\n * @copyright © ${year} ${author}. All rights reserved.\n */`;
-
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return;
+    const commands = [
+        {
+            name: 'extension.insertClassDocPhp',
+            lang: 'php'
+        },
+        {
+            name: 'extension.insertClassDocJS',
+            lang: 'javascript'
+        },
+        {
+            name: 'extension.insertMethodDocPhp',
+            lang: 'php'
+        },
+        {
+            name: 'extension.insertMethodDocJS',
+            lang: 'javascript'
+        },
+        {
+            name: 'extension.insertPropertyDocPhp',
+            lang: 'php'
+        },
+        {
+            name: 'extension.insertPropertyDocJS',
+            lang: 'javascript'
         }
+    ];
 
-        // const snippet = `/**\n * @author ${author}\n * @version ${version}\n */`;
-        editor.insertSnippet(new vscode.SnippetString(snippet));
-        vscode.window.showInformationMessage(snippet);
+    const config = vscode.workspace.getConfiguration('documate');
+    const author = config.get('author', 'Unknown Author');
+    const version = config.get('version', '1.0.0');
+    const email = config.get('email', 'email@example.com');
+    const year = new Date().getFullYear();
 
+    function getClassDocPhpSnippet() {
+        return `/**\n * Summary of class\n * @author ${author} <${email}>\n * @since ${year}\n * @version ${version}\n * @copyright © ${year} ${author}. All rights reserved.\n */`;
+    }
+
+    function getClassDocJsSnippet() {
+        return `/**\n * Summary of class\n * @author ${author} <${email}>\n * @since ${year}\n * @version ${version}\n * @copyright © ${year} ${author}. All rights reserved.\n */`;
+    }
+
+    function getDefaultDocSnippet() {
+        return `/**\n * NA\n */`;
+    }
+
+    commands.forEach(cmd => {
+        let disposable = vscode.commands.registerCommand(cmd.name, function () {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                return;
+            }
+
+            let snippet = '';
+
+            switch (cmd.name) {
+                case "extension.insertClassDocPhp":
+                    snippet = getClassDocPhpSnippet();
+                    break;
+
+                case "extension.insertClassDocJS":
+                    snippet = getClassDocJsSnippet();
+                    break;
+
+                default:
+                    snippet = getDefaultDocSnippet();
+                    break;
+            }
+
+            editor.insertSnippet(new vscode.SnippetString(snippet));
+            vscode.window.showInformationMessage(`${cmd.lang} - ${cmd.name} Documentation Added`);
+        });
+
+        context.subscriptions.push(disposable);
     });
-
-    context.subscriptions.push(disposable);
 }
 
 function deactivate() { }
